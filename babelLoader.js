c25b6a8ba1b77ca4b82b56d4252a1e30e48fa93b
Bryan Arendt <barendt@covermymeds.com>
2018-04-08 19:12:51 -0400

    webpack builds

---
+++ babelLoader.js
@@ -0,0 +1,12 @@
+const path = require("path");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const PATH = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const babelLoader = require("./babelLoader");
+const dist_path = PATH.resolve(__dirname, "app/dist");
+// uncomment to fail on compiler dep. warning
+// process.traceDeprecation = true;
+const baseConfig = {
+  mode: "production",
+  entry: "./app/app.js",
+  output: {
+    path: dist_path,
+    filename: "app.bundle.js",
+    publicPath: "/dist/"
+  },
+
+  plugins: [new webpack.NamedModulesPlugin()]
+};
+
+module.exports = function(env) {
+  const isDevelopment = env === "development";
+
+  console.log(
+    `This is a ${isDevelopment ? "development" : "production"} build`
+  );
+
+  if (isDevelopment) {
+    return merge(
+      baseConfig,
+      {
+        mode: "development",
+        devServer: {
+          contentBase: PATH.resolve(__dirname, "app"),
+          publicPath: "/dist/",
+          hotOnly: true,
+          overlay: true
+        },
+        plugins: [
+          new webpack.HotModuleReplacementPlugin(),
+          new webpack.DefinePlugin({
+            ENV_IS: JSON.stringify(isDevelopment ? "development" : "production")
+          })
+        ]
+      },
+      babelLoader
+    );
+  } else {
+    return merge(baseConfig, babelLoader);
+  }
+
+  return baseConfig;
+};
