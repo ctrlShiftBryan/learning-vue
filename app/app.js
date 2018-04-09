e7c1720c47b9923a6449292d719f6e9ae2064567
Bryan Arendt <barendt@covermymeds.com>
2018-04-08 23:47:39 -0400

    challenge 2 - ba - answers

--- app/app.js
+++ app/app.js
@@ -7,18 +7,79 @@ new Vue({
   el: '#app',
   data() {
     return {
-      counter: 0,
+      selected: '',
+      posts: [
+        {
+          author: '@vFitzgerald',
+          title: 'Quod Ducimus Omnis',
+          label: 'science',
+        },
+        {
+          author: '@mPalmer',
+          title: 'Vero Eius Laboriosam Ex Repudiandae',
+          label: 'math',
+        },
+        {
+          author: '@mDean',
+          title: 'Magnam Odit',
+          label: 'science',
+        },
+        {
+          author: '@tCole',
+          title: 'Velit Mollitia Voluptates Assumenda',
+          label: 'science',
+        },
+        {
+          author: '@jCooper',
+          title: 'Eum Commodi Cupiditate',
+          label: 'poetry',
+        },
+        {
+          author: '@lLamb',
+          title: 'Amet',
+          label: 'history',
+        },
+        {
+          author: '@fMartin',
+          title: 'Voluptatem Fuga Cum Asperiores Error',
+          label: 'science',
+        },
+        {
+          author: '@eHayes',
+          title: 'Ipsa Consectetur Rerum Repellat Quia',
+          label: 'math',
+        },
+        {
+          author: '@cCollier',
+          title: 'Dolor Nihil Delectus',
+          label: 'science',
+        },
+        {
+          author: '@cBenson',
+          title: 'Labore Ipsum Ad Pariatur',
+          label: 'poetry',
+        },
+      ],
+      newTitle: '',
+      newAuthor: '',
+      newLabel: '',
     };
   },
   methods: {
-    countup() {
-      this.counter++;
+    // no arrow function because we need this
+    submitForm() {
+      this.posts.push({
+        author: this.newAuthor,
+        label: this.newLabel.toLowerCase(),
+        title: this.newTitle,
+      });
     },
   },
   computed: {
-    // another view on the same data
-    countupComp() {
-      return this.counter + 1;
+    filteredPosts() {
+      return this.selected
+        ? this.posts.filter(x => x.label == this.selected.toLowerCase())
+        : this.posts;
     },
   },
 });
