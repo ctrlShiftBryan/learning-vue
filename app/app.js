7291fac547099d3889bb71ec336181601c6b080d
Bryan Arendt <barendt@covermymeds.com>
2018-04-10 07:14:05 -0400

    refactor component - after 2

--- app/app.js
+++ app/app.js
@@ -4,8 +4,22 @@ import { TweenMax } from 'gsap';
 import _ from 'lodash';
 Vue.config.devtools = true;
 
+// local component option
+// const IndividualComment = {
+//   template: '#comment-template',
+//   props: ['commentpost'],
+// };
+
+// AND then add to the components like this
+// new Vue({
+//   ...
+//   components: {
+//     'individual-comment': IndividualComment
+//   }
+// })
+
 Vue.component('individual-comment', {
-  template: `<li> {{ commentpost }} </li>`,
+  template: '#comment-template',
   props: ['commentpost'],
 });
 
@@ -13,11 +27,36 @@ new Vue({
   el: '#app',
   data: {
     newComment: '',
-    comments: ['Looks great Julianne!', 'I love the sea', 'Where are you at?'],
+    comments: [
+      {
+        text: 'Looks great Julianne!',
+        author: 'Robin Rendle',
+        authorImg:
+          'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-coffee.jpg',
+      },
+      {
+        text: 'I love the Sea',
+        author: 'Miriam Suzanne',
+        authorImg:
+          'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-miriam.jpg',
+      },
+      {
+        text: 'Where are you?',
+        author: 'Geoff Graham',
+        authorImg:
+          'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg',
+      },
+    ],
   },
   methods: {
     addComment: function() {
-      this.comments.push(this.newComment);
+      const newCommentObj = {
+        text: this.newComment,
+        author: 'Magoo',
+        authorImg:
+          'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-skull.jpg',
+      };
+      this.comments.push(newCommentObj);
       this.newComment = '';
     },
   },
