c96229e96cfed4a3f924237b2ee9eec4bac47c26
Bryan Arendt <barendt@covermymeds.com>
2018-04-10 08:11:32 -0400

    challenge 3 - ba

--- app/app.js
+++ app/app.js
@@ -4,35 +4,37 @@ import { TweenMax } from 'gsap';
 import _ from 'lodash';
 Vue.config.devtools = true;
 
-const app = new Vue({
+Vue.component('place', {
+  template: '#place-template',
+  props: ['name', 'img', 'desc'],
+});
+
+new Vue({
   el: '#app',
   data() {
     return {
-      selected: 'appBlack',
-      label: 'Label Name',
-      wineFont: 'Montserrat',
-      flourish: 'none',
-      wineImg: 'none',
-      imgOpacity: 0.8,
-      labelPlacement: 0,
-      labelColor: '#000000',
+      locations: [
+        {
+          name: 'moscow',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`,
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`,
+        },
+        {
+          name: 'rome',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`,
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`,
+        },
+      ],
     };
-  },
-  components: {
-    appBlack: {
-      template: '#black',
-    },
-    appWhite: {
-      template: '#white',
-    },
-    appFlor1: {
-      template: '#flor1',
-    },
-    appFlor2: {
-      template: '#flor2',
-    },
-    appFlor3: {
-      template: '#flor3',
-    },
   },
 });
