0e4cb0d4414be699f67b6d812463756ba817bd59
Bryan Arendt <barendt@covermymeds.com>
2018-04-11 06:22:48 -0400

    convert to built component app

---
+++ app/components/Place.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="place">
+    <img :src="img" width="235" height="300"/>
+    <!-- this gets filled out by slot -->
+    <slot></slot>
+    <p>{{ desc }}</p>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: ['img', 'desc'],
+};
+</script>
+
+<style scoped>
+
+</style>
